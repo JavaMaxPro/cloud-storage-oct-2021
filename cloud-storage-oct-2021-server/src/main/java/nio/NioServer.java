0d450dc11a08ef748694372a0ea932c9bf569da2
Maksim <mgubkov@gmail>
2021-10-22 23:52:02 +0700

    NioServer ls and cat

--- cloud-storage-oct-2021-server/src/main/java/nio/NioServer.java
+++ cloud-storage-oct-2021-server/src/main/java/nio/NioServer.java
@@ -8,18 +8,25 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 public class NioServer {
 
+    private Path root;
     private ServerSocketChannel server;
     private Selector selector;
     private ByteBuffer buffer;
 
 
     public NioServer() throws IOException {
+        root = Path.of("cloud-storage-oct-2021");
         buffer = ByteBuffer.allocate(256);
         server = ServerSocketChannel.open(); // accept -> SocketChannel
         server.bind(new InetSocketAddress(8182));
@@ -49,28 +56,76 @@ public class NioServer {
 
         StringBuilder sb = new StringBuilder();
 
-        while (true){
+        while (true) {
             int read = channel.read(buffer);
-            if(read == -1){
+            if (read == -1) {
                 channel.close();
                 return;
             }
 
-            if(read == 0){
+            if (read == 0) {
                 break;
             }
 
             buffer.flip();
-            while(buffer.hasRemaining()){
-                sb.append((char)buffer.get());
+            while (buffer.hasRemaining()) {
+                sb.append((char) buffer.get());
             }
             buffer.clear();
+            Path filePath;
+            String result = sb.toString();
+            String[] arrResult = result.split(" ");
+            arrResult[0].trim();
+            System.out.println(arrResult[0]);
+            if (arrResult[0].equals("ls")) {
+                String fileList = Files.list(root)
+                        .map(this::mapper)
+                        .collect(Collectors.joining("\n")) + "\n";
+                printCurrentDirName(channel);
+                System.out.println(arrResult[0]);
+            } else {
+                if (arrResult[0].equals("cat"))  {
+                    filePath = root.resolve(arrResult[1]);
+                    if (Files.exists(filePath)) {
+                        List<String> fileReadLine = Files.readAllLines(filePath, StandardCharsets.UTF_8);
+                        for (String s : fileReadLine) {
+                            channel.write(ByteBuffer.wrap(s.getBytes(StandardCharsets.UTF_8)));
+                        }
+                    }
+                } else {
+                    channel.write(ByteBuffer.wrap("No command \n\r".getBytes(StandardCharsets.UTF_8)));
+                    printCurrentDirName(channel);
+                    break;
+                }
+            }
+/*                case "cd":
+                    break;
+                case "touch":
+                    break;*/
 
-            String result = "[From serever]: " + sb.toString();
 
             channel.write(ByteBuffer.wrap(result.getBytes(StandardCharsets.UTF_8)));
+            printCurrentDirName(channel);
         }
 
+    }
+
+    private void printCurrentDirName(SocketChannel channel) throws IOException {
+        channel.write(ByteBuffer.wrap(root.toAbsolutePath().getFileName().toString().getBytes(StandardCharsets.UTF_8)));
+    }
+
+    private String mapper(Path path) {
+
+        if (Files.isDirectory(root)) {
+            return String.format("DIR " + path.getFileName().toString());
+        } else {
+            try {
+                long size = Files.size(root);
+                return String.format("FILE " + path.getFileName().toString() + " bytes");
+            } catch (Exception e) {
+                throw new RuntimeException("path not exist");
+            }
+        }
     }
 
     private void handleAccept(SelectionKey key) throws IOException {
@@ -78,6 +133,8 @@ public class NioServer {
         SocketChannel channel = server.accept();
         channel.configureBlocking(false);
         channel.register(selector, SelectionKey.OP_READ);
+        channel.write(ByteBuffer.wrap("Hi , this is telnet \n\r".getBytes(StandardCharsets.UTF_8)));
+        printCurrentDirName(channel);
     }
 
     public static void main(String[] args) throws IOException {
